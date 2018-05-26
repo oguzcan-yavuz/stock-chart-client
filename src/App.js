5f679faacb0e50b5a81a2a7529e1a71db6139513
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-05-27 02:58:07 +0300

    implementing chart.js to the app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Chart from 'chart.js';
+import Stocks from './components/Stocks.js';
+import Footer from './components/Footer.js';
+import { WS_URI } from './config.json';
 import './App.css';
-const WS_URI = 'ws://localhost:8080';
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { stocks: {} };
+  constructor() {
+    super();
+    this.chart = undefined;
+    this.state = { chartData: {}, ws: undefined };
+  }
+
+  componentDidMount() {
     this.initializeWebSocket();
   }
 
   initializeWebSocket() {
     let ws = new WebSocket(WS_URI);
-    ws.onopen = () => {
-      ws.send(JSON.stringify({ operator: 'add', symbol: 'aapl' }));
-    };
     ws.onmessage = (event) => {
       let stocks = JSON.parse(event.data);
       if(!stocks.error)
-        this.setState({ stocks: stocks });
-      console.log("state:", this.state);
-    }
+        this.updateChartData(stocks);
+    };
+    this.setState({ ws: ws });
   }
 
-  addStock() {
-
+  drawChart() {
+    if(this.chart !== undefined)
+      this.chart.destroy();
+    let ctx = document.getElementById('stockChart').getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data: this.state.chartData,
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Stocks'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    });
   }
 
-  deleteStock() {
-
+  updateChartData(stockData) {
+    console.log("stockData:", stockData);
+    let dateLabels = stockData[0].data.map(stockData => stockData.date).reverse();
+    console.log("date label:", dateLabels);
+    let chartData = this.state.chartData;
+    chartData.labels = dateLabels;
+    chartData.datasets = stockData.map(stock => {
+      return {
+        label: stock.symbol,
+        data: stock.data.map(stockData => stockData.open),
+        fill: false,
+        borderColor: 'lightgreen',
+        backgroundColor: 'green',
+      }
+    });
+    this.setState({ chartData: chartData });
+    console.log("state:", this.state);
+    this.drawChart();
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="chartWrapper">
+          <canvas id="stockChart" height="80"/>
+        </div>
+        <div className="stocks">
+          <Stocks ws={ this.state.ws }/>
+        </div>
+        <div>
+          { Footer() }
+        </div>
       </div>
-    );
+    )
   }
 }
 
