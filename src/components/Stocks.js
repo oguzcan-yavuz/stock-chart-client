7207e3d4fd51fa1b5ec07e9c80a56de493f5cab4
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-05-27 13:21:28 +0300

    stocks component completed, now it has both add and delete functionality

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -3,30 +3,37 @@ import React, { Component } from 'react';
 class Stock extends Component {
   constructor(props) {
     super(props);
-    this.state = { ws: props.ws };
+    this.state = { ws: props.ws, symbols: props.symbols };
+  }
+
+  componentDidMount() {
+    document.getElementById('stockSymbol')
+      .addEventListener("keyup", (event) => {
+        event.preventDefault();
+        if(event.key === "Enter")
+          document.getElementById('submitAdd').click();
+      });
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ ws: nextProps.ws });
-    console.log("stocks state:", this.state);
+    this.setState({ ws: nextProps.ws, symbols: nextProps.symbols });
   }
 
-  postStock(operator) {
-    let symbol = document.getElementById('stockSymbol').value;
-    console.log("postStock()");
-    console.log({ operator: operator, symbol: symbol });
+  postStock(operator, symbol=document.getElementById('stockSymbol').value) {
     this.state.ws.send(JSON.stringify({ operator: operator, symbol: symbol }));
+    document.getElementById('stockSymbol').value = '';
   }
 
   render() {
     return (
       <div>
-        <div className="stocksList">
-
+        <div className="deleteStock">
+          <p>Click on the stocks you want to delete</p>
+          { this.state.symbols.map((symbol, i) => <input type="submit" key={ i } onClick={ () => this.postStock('DELETE', symbol) } value={ symbol }/>) }
         </div>
         <div className="addStock">
           <input type="text" id="stockSymbol" placeholder="Stock symbol"/>
-          <input type="submit" onClick={ () => this.postStock('ADD') } value="Add Stock"/>
+          <input type="submit" id="submitAdd" onClick={ () => this.postStock('ADD') } value="Add Stock"/>
         </div>
       </div>
     )
